4c41727bcf97ddf315e525b9ca9341184f1c3e1b
Jo <jordanmoret4@gmail.com>
2025-07-13 14:18:14 +0200

    capitalize

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -3,15 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jordanmoret <jordanmoret@student.42.fr>    +#+  +:+       +#+        */
+/*   By: jmoret <jmoret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/16 23:02:18 by jordanmoret       #+#    #+#             */
-/*   Updated: 2025/06/16 23:02:19 by jordanmoret      ###   ########.fr       */
+/*   Created: 2024/09/19 08:25:09 by jmoret            #+#    #+#             */
+/*   Updated: 2024/09/25 07:34:24 by jmoret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_is_alphanum(char c)
+/* #include <stdio.h>  */
+
+int	ft_is_alphanum(char c)
 {
-	return (( c >= '0' && c <= '9')
-			|| (c >= 'A' && c <= 'Z')
-			|| (c >= 'a' && c <=)
+	return ((c >= '0' && c <= '9')
+		|| (c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z'));
+}
+
+char	ft_uppercase(char c)
+{
+	return (c - (c >= 'a' && c <= 'z') * 32);
+}
+
+char	ft_lowercase(char c)
+{
+	return (c + (c >= 'A' && c <= 'Z') * 32);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (i == 0 || !ft_is_alphanum(str[i - 1]))
+			str[i] = ft_uppercase(str[i]);
+		else
+			str[i] = ft_lowercase(str[i]);
+		++i;
+	}
+	return (str);76
+}
+/*int	main(void)
+	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	ft_strcapitalize(str);
+	printf("%s\n", str);
+	return (0);
+}*/
+
+/*int    ft_is_alphanum(char c)
+ {
+     if ((c >= '0' && c <= '9')
+         || (c >= 'A' && c <= 'Z')
+         || (c >= 'a' && c <= 'z'))
+         return (1);
+     return (0);
+ }
+
+ char    ft_uppercase(char c)
+ {
+     if (c >= 'a' && c <= 'z')
+         return (c - 32);
+     return (c);
+ }
+
+ char    ft_lowercase(char c)
+ {
+     if (c >= 'A' && c <= 'Z')
+         return (c + 32);
+     return (c);
+ }
+
+ char    *ft_strcapitalize(char *str)
+ {
+     int    i;
+
+     i = 0;
+     while (str[i] != '\0')
+     {
+         if (i == 0 || !ft_is_alphanum(str[i - 1]))
+             str[i] = ft_uppercase(str[i]);
+         else
+             str[i] = ft_lowercase(str[i]);
+         i++;
+     }
+     return (str);
+ }
+*/
--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -3,10 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jordanmoret <jordanmoret@student.42.fr>    +#+  +:+       +#+        */
+/*   By: jmoret <jmoret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/16 23:02:21 by jordanmoret       #+#    #+#             */
-/*   Updated: 2025/06/16 23:02:22 by jordanmoret      ###   ########.fr       */
+/*   Created: 2024/09/19 08:26:24 by jmoret            #+#    #+#             */
+/*   Updated: 2024/09/24 14:24:45 by jmoret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlen(char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = 0;
+	return (ft_strlen(src));
+}
+/* #include <stdio.h>
+
+int	main(void)
+{
+	char			src[] = "Hello, world!";
+	char			dest[20];
+	unsigned int	len;
+
+	len = ft_strlcpy(dest, src, sizeof(dest));
+	printf("Dest: %s\n", dest);
+	printf("Source len: %u\n", len);
+	return (0);
+} */
