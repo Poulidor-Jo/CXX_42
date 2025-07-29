f57148f6ac7102508238179bf2fcc2530605a42d
Jo <jordanmoret4@gmail.com>
2025-07-29 20:38:02 +0200

    add

---
+++ Training/itoa.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	count_digits(int n)
+{
+	int	count = 0;
+	if (n >= 0 && n <= 0)
+		return (1);
+	if (n < 0)									// if negative will add +1 for the sign '-'
+		count++;
+	while (n != 0)
+	{
+		count++;
+		n = n / 10;
+	}
+	return (count);
+}
+char	*ft_itoa(int n)
+{
+	int		n_len;
+	long	nb;
+
+	nb = n;
+	n_len = count_digits(nb);
+	char *dest = malloc(sizeof(char) * n_len + 1);
+	if (!dest)
+		return (NULL);
+	dest[n_len] = '\0';
+	if (nb == 0)
+		dest[0] = '0';
+	if (nb < 0)
+	{
+		dest[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		dest [n_len - 1] = (nb % 10) + '0';
+		nb /= 10;
+		n_len--;
+	}
+	return (dest);
+}
+
+//   ---------------------
+//   ::  test it dear   :: 
+//   ---------------------
+
+#include <stdio.h>
+int main()
+{
+int test_values[] = {0,1,5,9, -123, 456, -7890, 2147483647, -2147483648};
+int num_tests = sizeof(test_values) / sizeof(test_values[0]);
+
+for (int i = 0; i < num_tests; i++)
+{
+int value = test_values[i];
+char *result = ft_itoa(value);
+printf("ft_itoa(%d) = %s\n", value, result);
+    free(result);
+}
+return 0;
+}
---
+++ Training/split.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+char		**ft_split(char *str)
+{
+	int		i = 0;                                                              // index of source string
+	int		j = 0;                                                              // index of new each string in the array of strings
+	int		dest_index = 0;                                                     // dest/vector index
+	char	**dest;
+
+	dest = malloc(sizeof(char *) * 256);                                        // creates 256 vectors in dest**
+	if (dest == NULL)					
+		return (NULL);
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')                   //* search for the first caracter (only ||'s)
+		i++;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		dest[dest_index] = malloc(sizeof(char) * 4096);                         // creates empty string of size 4096
+		if (dest[dest_index] == NULL)
+			return (NULL);
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])     //* loop only on caracters (only &&'s)
+		{
+			dest[dest_index][j] = str[i];                                       // fill the empty string
+			j++;
+			i++;
+		}
+		dest[dest_index][j] = '\0';                                             // add NULL / '\0' at the end of the string
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')               //* search for the next word (only ||'s)
+			i++;
+		dest_index++;                                                           // next vector
+	}
+	dest[dest_index] = NULL;                                                    // at the end the last vector is null
+	return (dest);                                            
+}
+
+//   ---------------------
+//   ::  test it dear   :: 
+//   ---------------------
+
+#include <stdio.h>
+int main(int ac, char **av)
+{
+    char *src = av[1];
+    char **dest = ft_split(src);
+    int i = 0;
+
+    while (dest[i])
+    {
+        printf("dest[%d]: %s\n", i, dest[i]);
+        i++;
+    }
+    return 0;
+}
